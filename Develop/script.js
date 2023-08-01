5b4c275d1d4f2752a9b8ee6315fa1c3506e72102
Michael Leblanc <mleblanc1@werfen.com>
2023-08-01 16:10:28 -0400

    added primary functions which make the application work as expected

--- Develop/script.js
+++ Develop/script.js
@@ -2,25 +2,72 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  let dayData = dayjs()
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+ 
+ let btns = $('.saveBtn')
+ btns.each(function() {
+  let id = $(this).data().id
+  $(this).on('click', function(){
+    let value = $(`#hour-${id} textarea`).val();
+    const valueString = JSON.stringify(value);
+    localStorage.setItem(`hour-${id}`, valueString);
+  });
+
+  let storedUserData = localStorage.getItem(`hour-${id}`);
+  let parsedUserData = JSON.parse(storedUserData)
+    $(`#hour-${id} textarea`).val(parsedUserData);
+ })
+ 
+//A test used to see if my checkTime function is working correctly to remove classes that are no longer needed
+
+//  let currentHour = 9;
+//  setInterval(() => {
+//   console.log(currentHour);
+//   currentHour++;
+//   if (currentHour > 15) {
+//     currentHour = 9;
+//   }
+//  }, 1000)
+
+  function checkTime() {
+    let currentHour = dayData.hour();
+    for (let i = 9; i <= 17; i++) {
+      if (currentHour === i) {
+     $(`#hour-${i}`).removeClass("future past");
+        $(`#hour-${i}`).addClass("present");
+      }
+      if (currentHour < i) {
+        $(`#hour-${i}`).removeClass("present past");
+        $(`#hour-${i}`).addClass("future");
+      }
+      if (currentHour > i) {
+        $(`#hour-${i}`).removeClass("future present");
+        $(`#hour-${i}`).addClass("past");
+      }
+    }
+  }
+  setInterval(checkTime, 100)
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  checkTime();
+
+  //Adds the current day and time to page while also refreshing the time every 60 seconds
+  setInterval(() => {
+    $('#currentDay').html(dayjs().format('dddd, MMMM D, YYYY h:mm A'));
+  }, 60000);
   $('#currentDay').html(dayjs().format('dddd, MMMM D, YYYY h:mm A'));
-  console.log(dayjs($h))
 });
 
 
